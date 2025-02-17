bfd232329d4d3d232a5977514cbc3ca4b28adc68
Harsh Mathur <harsh.mathur@mobileprogramming.com>
2025-02-17 12:30:57 +0530

    add space no change

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use("/api/enquiries", enquiryRoutes);
 // Default route to check API status
 app.get('/', (req, res) => {
   res.send('API is running...');
-});
+}); 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
